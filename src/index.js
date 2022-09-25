9ff8e9d8551eaa47974bb2227e1b93c85c3a50b9
Dang Huy <nguyendanghuy481@gmail.com>
2022-09-25 21:30:59 +0700

    Cấu hình sử dụng CSS, SASS

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from '~/App';
+import Globalstyles from './Combonent/Globalstyles';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App />
+        <Globalstyles>
+            <App />
+        </Globalstyles>
     </React.StrictMode>,
 );
 
